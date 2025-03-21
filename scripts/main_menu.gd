da3e68a910f22e9bf9127a54f7c9bc6f999249d7
glglak <karem.kamel@gmail.com>
2025-03-22 01:02:13 +0200

    Update main_menu.gd with proper methods

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,30 +1,30 @@
 extends Control
 
 # References to UI elements
-@onready var play_button: Button = $PlayButton
-@onready var quit_button: Button = $QuitButton
-@onready var high_score_label: Label = $HighScoreLabel
+@onready var play_button = $PlayButton
+@onready var quit_button = $QuitButton
+@onready var high_score_label = $HighScoreLabel
 
-# Save file path
+# Save file path - same as in game_manager.gd
 const SAVE_FILE_PATH = "user://highscore.save"
 
-func _ready() -> void:
+func _ready():
 	# Connect button signals
-	play_button.connect("pressed", _on_play_button_pressed)
-	quit_button.connect("pressed", _on_quit_button_pressed)
+	play_button.connect("pressed", Callable(self, "_on_play_button_pressed"))
+	quit_button.connect("pressed", Callable(self, "_on_quit_button_pressed"))
 	
 	# Display high score
 	load_and_display_high_score()
 
-func _on_play_button_pressed() -> void:
-	# Start the game
+func _on_play_button_pressed():
+	# Change to the game scene
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 
-func _on_quit_button_pressed() -> void:
+func _on_quit_button_pressed():
 	# Quit the game
 	get_tree().quit()
 
-func load_and_display_high_score() -> void:
+func load_and_display_high_score():
 	var high_score = 0
 	
 	# Try to load high score from file
@@ -38,4 +38,4 @@ func load_and_display_high_score() -> void:
 				high_score = save_data.high_score
 	
 	# Update the high score display
-	high_score_label.text = "High Score: %d" % high_score
+	high_score_label.text = "High Score: " + str(high_score)
