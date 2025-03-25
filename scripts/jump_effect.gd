18d51c3492508e0012d90af5641975d57eb63f31
glglak <karem.kamel@gmail.com>
2025-03-26 00:28:56 +0200

    Completely emptied jump_effect.gd script to remove all CPUParticles2D functionality

--- scripts/jump_effect.gd
+++ scripts/jump_effect.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
-@onready var particles = $CPUParticles2D if has_node("CPUParticles2D") else null
+# Extremely simplified jump effect script
+# Just a placeholder to avoid errors
 
-# Called when the node enters the scene tree for the first time
 func _ready():
-	# Just make sure we're not auto-emitting
-	if particles:
-		particles.emitting = false
+	# Do nothing
+	pass
 
-# Simple function to play the effect - no complex property handling
 func play_jump_effect():
-	if particles:
-		# Just toggle emission - the scale is set in the scene directly
-		particles.emitting = true
+	# Do nothing
+	pass
